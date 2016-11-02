49a7a1b73022394b4a59cf04d1f7434ed0be31bf
SignalFlagZ <Signal0Flag0Z@gmail.com>
2016-11-02 13:54:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,27 +8,20 @@
 cd `dirname $0`
 sudo apt-get update
 sudo apt-get -y upgrade
-
 #
 sudo mv /etc/udhcpd.conf /etc/udhcpd.conf.bk
 sudo cp udhcpd.conf /etc/
-
 #
 sudo sed -i.bak -e "s/^\(DHCPD_ENABLED\).*/#\1/g" /etc/default/udhcpd
-
 #
 sudo cp hostapd.conf /etc/hostapd/
 sudo cp /etc/default/hostapd /etc/default/hostapd.bk
 sudo echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd
-
 #
 sudo systemctl disable hostapd
 sudo systemctl disable udhcpd
-
 #
 sudo cp /etc/network/interfaces /etc/network/interfaces.bk
 sudo cp interfaces /etc/network/
-
 #
 sudo cp apscan.sh /usr/local/bin/
-
