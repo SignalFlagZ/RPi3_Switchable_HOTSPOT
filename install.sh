135fb91fc5681a314c7e5eaa2f1e12f9864d9757
SignalFlagZ <Signal0Flag0Z@gmail.com>
2016-11-02 12:46:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 # Install RPi3_Switchable_HOTSPOT
 #
+cd `dirname $0`
 sudo apt-get update
 sudo apt-get -y upgrade
 
 #
 sudo mv /etc/udhcpd.conf /etc/udhcpd.conf.bk
-sudo cp udhcpd.conf /etc/udhcpd.conf
+sudo cp udhcpd.conf /etc/
 
 #
 sudo sed -i.bak -e "s/^\(DHCPD_ENABLED\).*/#\1/g" /etc/default/udhcpd
 
 #
-sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp hostapd.conf /etc/hostapd/
 sudo cp /etc/default/hostapd /etc/default/hostapd.bk
 sudo echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd
 
@@ -22,5 +23,8 @@ sudo systemctl disable udhcpd
 
 #
 sudo cp /etc/network/interfaces /etc/network/interfaces.bk
-sudo cp interfaces /etc/network/interfaces
+sudo cp interfaces /etc/network/
+
+#
+sudo cp apscan.sh /usr/local/bin/
 
